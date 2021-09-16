518419bc29aaeabb8454daa5f651fb7449fe475f
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-16 19:22:39 +0700

    Initial commit

---
+++ app/src/main/java/com/rujirakongsomran/bottomnavigationwithbadges/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.rujirakongsomran.bottomnavigationwithbadges
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rujirakongsomran.bottomnavigationwithbadges.ui.theme.BottomNavigationWithBadgesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomNavigationWithBadgesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BottomNavigationWithBadgesTheme {
+        Greeting("Android")
+    }
+}
